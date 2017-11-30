f8ba24e909f2b9984a359bc1b7c8ff22445f83ff
heyMP <mgp140@gmail.com>
2017-11-30 09:31:20 -0500

    made media manager app.

---
+++ core/dslmcode/profiles/elmsmedia-7.x-1.x/modules/apps/elmsmedia_mangager/apps/bower-empty.js
@@ -0,0 +1,16 @@
+// var fs = require('fs');
+// var glob = require("glob-fs")({ gitignore: true });
+
+console.log('test');
+
+// glob.readdir('bower_components/**/*.html', function (err, files) {
+//   console.log(files);
+//   if (err) return console.error(err);
+//   for (let index = 0; index < files.length; index++) {
+//     const element = files[index];
+//     fs.writeFile(element, ' ', (err) => {
+//       if (err) throw err;
+//       console.log(`${element} has been emptied`);
+//     })
+//   }
+// });
