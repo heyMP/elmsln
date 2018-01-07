a2e100b9e657b23e7429e82c7c783f3446bbbf37
Michael Potter <mgp140@gmail.com>
2018-01-07 15:28:01 -0500

    added updated, author info.

--- core/webcomponents/apps/elmsmedia-dashboard/elmsmedia-dashboard.php
+++ core/webcomponents/apps/elmsmedia-dashboard/elmsmedia-dashboard.php
@@ -12,6 +12,14 @@ require_once(__ROOT__.'/services/ELMSMediaService.php');
 function _elmsmedia_dashboard_data($machine_name, $app_route, $params, $args) {
   $media_service = new ELMSMediaService();
   $return = $media_service->getMedia($params);
+  // add last updated formatted date for this endpoint
+  foreach ($return['list'] as &$item) {
+    $changed = $item->meta->changed;
+    $date = strtotime($changed);
+    $formatedChanged = date('m/d/Y', $date);
+    $item->display = $item->display ?: new stdClass();
+    $item->display->updated = $formatedChanged;
+  }
   $return['status'] = 200;
   return $return;
 }
